2458f238f1f9a2ebf2e34fe4fe93ac4341a04bf3
Abraham Kinuthia <abrahamkinuthia193@gmail.com>
2024-07-12 14:15:57 +0300

    done

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///booknest.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'my_jwt_secret_key')
